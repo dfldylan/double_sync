61b99466dbe62bbccec78434dcbb222a632d4ea9
dfldylan <dfldylan@qq.com>
2023-08-08 20:20:39 +0800

    refact: content.js

--- js/content.js
+++ js/content.js
@@ -13,51 +13,51 @@ function getXPath(node) {
   var comp, comps = [];
   var parent = null;
   var xpath = '';
-  var getPos = function(node) {
-      var position = 1, curNode;
-      if (node.nodeType == Node.ATTRIBUTE_NODE) {
-          return null;
+  var getPos = function (node) {
+    var position = 1, curNode;
+    if (node.nodeType == Node.ATTRIBUTE_NODE) {
+      return null;
+    }
+    for (curNode = node.previousSibling; curNode; curNode = curNode.previousSibling) {
+      if (curNode.nodeName == node.nodeName) {
+        ++position;
       }
-      for (curNode = node.previousSibling; curNode; curNode = curNode.previousSibling) {
-          if (curNode.nodeName == node.nodeName) {
-              ++position;
-          }
-      }
-      return position;
-   }
+    }
+    return position;
+  }
 
   if (node instanceof Document) {
-      return '/';
+    return '/';
   }
 
   for (; node && !(node instanceof Document); node = node.nodeType == Node.ATTRIBUTE_NODE ? node.ownerElement : node.parentNode) {
-      comp = comps[comps.length] = {};
-      switch (node.nodeType) {
-          case Node.TEXT_NODE:
-              comp.name = 'text()';
-              break;
-          case Node.ATTRIBUTE_NODE:
-              comp.name = '@' + node.nodeName;
-              break;
-          case Node.PROCESSING_INSTRUCTION_NODE:
-              comp.name = 'processing-instruction()';
-              break;
-          case Node.COMMENT_NODE:
-              comp.name = 'comment()';
-              break;
-          case Node.ELEMENT_NODE:
-              comp.name = node.nodeName;
-              break;
-      }
-      comp.position = getPos(node);
+    comp = comps[comps.length] = {};
+    switch (node.nodeType) {
+      case Node.TEXT_NODE:
+        comp.name = 'text()';
+        break;
+      case Node.ATTRIBUTE_NODE:
+        comp.name = '@' + node.nodeName;
+        break;
+      case Node.PROCESSING_INSTRUCTION_NODE:
+        comp.name = 'processing-instruction()';
+        break;
+      case Node.COMMENT_NODE:
+        comp.name = 'comment()';
+        break;
+      case Node.ELEMENT_NODE:
+        comp.name = node.nodeName;
+        break;
+    }
+    comp.position = getPos(node);
   }
 
   for (var i = comps.length - 1; i >= 0; i--) {
-      comp = comps[i];
-      xpath += '/' + comp.name;
-      if (comp.position != null) {
-          xpath += '[' + comp.position + ']';
-      }
+    comp = comps[i];
+    xpath += '/' + comp.name;
+    if (comp.position != null) {
+      xpath += '[' + comp.position + ']';
+    }
   }
 
   return xpath;
@@ -67,56 +67,56 @@ function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-window.addEventListener("focus", function(event){
-  if(hover_element) {
+window.addEventListener("focus", function (event) {
+  if (hover_element) {
     hover_element.classList.remove("__hover_highlight__qcr");
   }
 });
 
-document.addEventListener('click', function(e) {
+document.addEventListener('click', function (e) {
   e = e || window.event;
   var target = e.target;
 
 }, false);
 
-document.addEventListener("mouseover", function(e){
+document.addEventListener("mouseover", function (e) {
   let t = e.target;
   console.log("hover element", t.nodeName);
   let x_path = getXPath(t);
-  if(t.nodeName !== "TABLE" && t.nodeName !== "HTML" && t.nodeName !== "BODY" && t.nodeName !== "IMG" ) {
-    chrome.runtime.sendMessage({event: "mouseover", url: window.location.href, xpath: x_path});
+  if (t.nodeName !== "TABLE" && t.nodeName !== "HTML" && t.nodeName !== "BODY" && t.nodeName !== "IMG") {
+    chrome.runtime.sendMessage({ event: "mouseover", url: window.location.href, xpath: x_path });
   }
 });
 
-window.addEventListener('scroll', function(e) {
-  if(pointer_clone.style.display == "none") {
-    chrome.runtime.sendMessage({event: "scroll", url: window.location.href, scrolly: window.scrollY});
+window.addEventListener('scroll', function (e) {
+  if (pointer_clone.style.display == "none") {
+    chrome.runtime.sendMessage({ event: "scroll", url: window.location.href, scrolly: window.scrollY });
   }
 });
 
-window.addEventListener('mousemove', function(e) {
+window.addEventListener('mousemove', function (e) {
   pointer_clone.style.display = "none";
-  chrome.runtime.sendMessage({event: "mousemove", url: window.location.href, client_x: e.clientX, client_y: e.clientY});
+  chrome.runtime.sendMessage({ event: "mousemove", url: window.location.href, client_x: e.clientX, client_y: e.clientY });
 });
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      pointer_clone.style.display = "block";
-      if(request.event == "scroll") {
-        window.scrollTo(0, request.scrolly);
-      } else if(request.event == "mousemove") {
-        pointer_clone.style.left = "" + request.client_x + "px";
-        pointer_clone.style.top = "" + request.client_y + "px";
-        console.log("Force set pointer positoin");
-      } else if(request.event == "mouseover") {
-        if(hover_element) {
-          hover_element.classList.remove("__hover_highlight__qcr");
-        }
-        let hover_e = getElementByXpath(request.xpath);
-        hover_e.classList.add("__hover_highlight__qcr");
-        hover_element = hover_e;
+  function (request, sender, sendResponse) {
+    pointer_clone.style.display = "block";
+    if (request.event == "scroll") {
+      window.scrollTo(0, request.scrolly);
+    } else if (request.event == "mousemove") {
+      pointer_clone.style.left = "" + request.client_x + "px";
+      pointer_clone.style.top = "" + request.client_y + "px";
+      console.log("Force set pointer positoin");
+    } else if (request.event == "mouseover") {
+      if (hover_element) {
+        hover_element.classList.remove("__hover_highlight__qcr");
       }
-
-      sendResponse();
+      let hover_e = getElementByXpath(request.xpath);
+      hover_e.classList.add("__hover_highlight__qcr");
+      hover_element = hover_e;
     }
-  );
+
+    sendResponse();
+  }
+);
